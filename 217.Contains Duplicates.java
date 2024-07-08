7e4031216f88b02edf3c52f62233f93a3475406c
Tramanata <102120863+Tramanata@users.noreply.github.com>
2024-07-08 16:23:09 -0400

    Update 217.Contains Duplicates.java

--- 217.Contains Duplicates.java
+++ 217.Contains Duplicates.java
@@ -1,1 +1,18 @@
+// This solutions uses a hashset to feed the values within the nums array into it
+// It then uses the functions defined for a HashSet (contains and add) to check if the value is in the set, if not, add that value to the set
+// This has a time complexity of ____ and a space compexit of ____
 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            int number = nums[i];
+            if(set.contains(number)){
+                return true;
+            }else{
+                set.add(number);
+            }
+        }
+        return false;
+    }
+}
